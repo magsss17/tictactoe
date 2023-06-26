97ab939b34f89e3de4cfdd898bf2c7d8982fc39c
Maggie <mschwier@wharton.upenn.edu>
2023-06-26 18:08:55 +0000

    exercise 1

--- student/exercises/src/tic_tac_toe_exercises_lib.ml
+++ student/exercises/src/tic_tac_toe_exercises_lib.ml
@@ -61,14 +61,24 @@ let non_win =
 
    After you are done with this implementation, you can uncomment out
    "evaluate" test cases found below in this file. *)
+
 let available_moves
   ~(game_kind : Game_kind.t)
   ~(pieces : Piece.t Position.Map.t)
   : Position.t list
   =
-  ignore game_kind;
-  ignore pieces;
-  failwith "Implement me!"
+  let dimension = match game_kind with Tic_tac_toe -> 3 | Omok -> 15 in
+  let l = List.init dimension ~f:(fun i -> i) in
+  let all_pos =
+    List.concat
+      (List.map l ~f:(fun x ->
+         List.map l ~f:(fun y -> { Position.row = x; column = y })))
+  in
+  let piece_pos = Map.keys pieces in
+  Set.to_list
+    (Set.diff
+       (Set.of_list (module Position) all_pos)
+       (Set.of_list (module Position) piece_pos))
 ;;
 
 (* Exercise 2.
@@ -208,16 +218,26 @@ let%expect_test "print_non_win" =
 ;;
 
 (* After you've implemented [available_moves], uncomment these tests! *)
-(* let%expect_test "yes available_moves" = let (moves : Position.t list) =
-   available_moves ~game_kind:non_win.game_kind ~pieces:non_win.pieces |>
-   List.sort ~compare:Position.compare in print_s [%sexp (moves : Position.t
-   list)]; [%expect {| (((row 0) (column 1)) ((row 0) (column 2)) ((row 1)
-   (column 1)) ((row 1) (column 2)) ((row 2) (column 1))) |}] ;;
+let%expect_test "yes available_moves" =
+  let (moves : Position.t list) =
+    available_moves ~game_kind:non_win.game_kind ~pieces:non_win.pieces
+    |> List.sort ~compare:Position.compare
+  in
+  print_s [%sexp (moves : Position.t list)];
+  [%expect
+    {| 
+   (((row 0) (column 1)) ((row 0) (column 2)) ((row 1) (column 1))
+    ((row 1) (column 2)) ((row 2) (column 1))) |}]
+;;
 
-   let%expect_test "no available_moves" = let (moves : Position.t list) =
-   available_moves ~game_kind:win_for_x.game_kind ~pieces:win_for_x.pieces |>
-   List.sort ~compare:Position.compare in print_s [%sexp (moves : Position.t
-   list)]; [%expect {| () |}] ;; *)
+let%expect_test "no available_moves" =
+  let (moves : Position.t list) =
+    available_moves ~game_kind:win_for_x.game_kind ~pieces:win_for_x.pieces
+    |> List.sort ~compare:Position.compare
+  in
+  print_s [%sexp (moves : Position.t list)];
+  [%expect {| () |}]
+;;
 
 (* When you've implemented the [evaluate] function, uncomment the next two
    tests! *)
--- student/src/tictactoe_game_ai.ml
+++ student/src/tictactoe_game_ai.ml
@@ -14,9 +14,10 @@ let random_move_strategy
   ~(pieces : Piece.t Position.Map.t)
   : Position.t
   =
-  ignore game_kind;
-  ignore pieces;
-  failwith "Implement me!"
+  let avail = Tic_tac_toe_exercises_lib.available_moves ~game_kind ~pieces in
+  if List.length avail = 0
+  then failwith "no available positions"
+  else List.random_element_exn avail
 ;;
 
 (* Exercise 3.2.
@@ -89,8 +90,8 @@ let _ = score
 let compute_next_move ~(me : Piece.t) ~(game_state : Game_state.t)
   : Position.t
   =
-  ignore random_move_strategy;
   ignore me;
-  ignore game_state;
-  { Position.row = 0; column = 0 }
+  random_move_strategy
+    ~game_kind:game_state.game_kind
+    ~pieces:game_state.pieces
 ;;
